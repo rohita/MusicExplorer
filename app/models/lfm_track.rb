787b969fdb852406b3a7a7c05bf2f661b92cea3c
Rohit Agarwal <rohit.net@gmail.com>
2010-03-06 18:57:02 -0800

    Added similar tracks to the Track page.

--- app/models/lfm_track.rb
+++ app/models/lfm_track.rb
@@ -10,6 +10,7 @@ class LfmTrack
   attr_accessor :image_url
   attr_accessor :description
   attr_accessor :similar_tracks
+  attr_accessor :similar_url
   
   def self.find(track_name, artist_name)
     track_name_encoded = URI.escape(track_name, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
@@ -29,6 +30,23 @@ class LfmTrack
     track.album_name = doc.xpath("lfm/track/album/title").first.content
     track.image_url = doc.xpath("lfm/track/album/image[@size='extralarge']").first.content
 
+    lfm_url = "#{LASTFM_URL}?method=track.getsimilar&api_key=#{LASTFM_KEY}"
+    simtracks_url = "#{lfm_url}&artist=#{artist_name_encoded}&track=#{track_name_encoded}"
+    doc = Nokogiri::XML(open(simtracks_url))
+    
+    i = 0
+    track.similar_tracks = []
+    doc.xpath('/lfm/similartracks/track').each do |track_node|
+      simtrack = LfmTrack.new
+      simtrack.name = track_node.xpath("name").inner_text
+      simtrack.duration = track_node.xpath("duration").inner_text
+      simtrack.artist_name = track_node.xpath("artist/name").inner_text
+      simtrack.image_url = track_node.xpath("image[@size='small']").inner_text
+      track.similar_tracks << simtrack
+      i += 1
+      break if i == 10
+    end
+    track.similar_url = simtracks_url
     return track
   end
   
